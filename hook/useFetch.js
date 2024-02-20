911286732f53c5ae9fa8a2d285da2bd21dead60b
Aakash Suresh <aakashakshith455@gmail.com>
2024-02-21 01:38:56 +0530

    Developed till Popular Jobs

---
+++ hook/useFetch.js
@@ -0,0 +1,52 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+const useFetch = (endpoint, query) => {
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const options = {
+    method: "GET",
+    url: `https://jsearch.p.rapidapi.com/${endpoint}`,
+    headers: {
+      "X-RapidAPI-Key": 'c29e14c12amshc0706ba1db29f4ap1ae7a3jsn2c4f17d0c296',
+      "X-RapidAPI-Host": "jsearch.p.rapidapi.com",
+    },
+    params: { ...query },
+  };
+
+  const fetchData = async () => {
+    setIsLoading(true);
+
+    try {
+      const response = await axios.request(options);
+
+      setData(response.data.data);
+    } catch (error) {
+      setError(error);
+      console.log(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    let isMounted = true;
+
+    fetchData();
+
+    return () => {
+      isMounted = false;
+    };
+  }, [options]); // Include options in the dependency array
+
+  const refetch = () => {
+    setIsLoading(true);
+    fetchData();
+  };
+
+  return { data, isLoading, error, refetch };
+};
+
+export default useFetch;
