8d06be325082ffa3067ee0f28b507b35af2cfa68
Aakash Suresh <aakashakshith455@gmail.com>
2024-02-20 22:22:41 +0530

    Developed Welcome Screen

--- app/index.js
+++ app/index.js
@@ -1,21 +1,34 @@
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
-import Data from '../screen/data';
+import { View,Text,ScrollView,SafeAreaView } from "react-native";
+import { useState } from "react";
+import { Stack, useRouter } from "expo-router";
 
-export default function App() {
-  return (
-    <SafeAreaView>
-      <View>
-        <Data/>
-      </View>
-    </SafeAreaView>
-  );
+import {COLORS,icons,images,SIZES} from '../constants/';
+import {Nearbyjobs,Popularjobs,ScreenHeaderBtn,Welcome} from '../components'
+const Home = () =>{
+  const router = useRouter();
+
+  return(
+    <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
+      <Stack.Screen
+        options={{
+          headerStyle: { backgroundColor: COLORS.lightWhite },
+          headerShadowVisible: false,
+          headerLeft: () => (<ScreenHeaderBtn iconUrl={icons.menu} dimension="60%" />),
+          headerRight: () => (<ScreenHeaderBtn iconUrl={images.profile} dimension="100%" />),
+          headerTitle: ''
+        }}
+      />
+
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <View style={{flex:1,padding:SIZES.medium}}>
+          <Welcome/>
+          <Popularjobs/>
+          <Nearbyjobs/>
+        </View>
+      </ScrollView>
+  </SafeAreaView>
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default Home;
+
